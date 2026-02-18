79617feed9b5d53e8b924e2d596f633a248bd037
praveen <psagra13@gmail.com>
2026-02-19 01:30:03 +0530

    added createJobBean class and generic csv reader

--- src/test/java/com/api/utils/CSVReaderUtil.java
+++ src/test/java/com/api/utils/CSVReaderUtil.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 import com.dataproviders.api.bean.UserBean;
 import com.opencsv.CSVReader;
+import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
 
 public class CSVReaderUtil {
@@ -14,29 +15,21 @@ public class CSVReaderUtil {
     private CSVReaderUtil() {
     }
 
-    public static Iterator<UserBean> loadCSV(String pathOfCSVFile) {
+    public static <T>Iterator<T> loadCSV(String pathOfCSVFile,Class<T> bean) {
+    	
+    	InputStream inputSteam = Thread.currentThread().getContextClassLoader().getSystemResourceAsStream(pathOfCSVFile);
+    	InputStreamReader inputStreamReader = new InputStreamReader(inputSteam);
+    	CSVReader csvReader = new CSVReader(inputStreamReader);
+    	CsvToBean<T> csvToBean = new CsvToBeanBuilder(csvReader)
+    			.withType(bean)
+    			.withIgnoreEmptyLine(true)
+    			.build();
+    	
+    	List<T>list = csvToBean.parse();
+    	return list.iterator();
+    	
 
-        InputStream inputStream = Thread.currentThread()
-                .getContextClassLoader()
-                .getResourceAsStream(pathOfCSVFile);
-
-        if (inputStream == null) {
-            throw new RuntimeException("CSV file not found at path: " + pathOfCSVFile);
-        }
-
-        try (InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
-             CSVReader csvReader = new CSVReader(inputStreamReader)) {
-
-            List<UserBean> userCred = new CsvToBeanBuilder<UserBean>(csvReader)
-                    .withType(UserBean.class)
-                    .withIgnoreEmptyLine(true)
-                    .build()
-                    .parse();
-
-            return userCred.iterator();
-
-        } catch (Exception e) {
-            throw new RuntimeException("Failed to parse CSV file: " + pathOfCSVFile, e);
-        }
-    }
+        
+}
+    
 }
