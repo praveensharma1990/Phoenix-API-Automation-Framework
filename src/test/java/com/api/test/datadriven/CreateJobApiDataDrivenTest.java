3693d93eec1a93546b655698e8a72742cc46460d
praveen <psagra13@gmail.com>
2026-02-23 23:22:39 +0530

    wip

--- src/test/java/com/api/test/datadriven/CreateJobApiDataDrivenTest.java
+++ src/test/java/com/api/test/datadriven/CreateJobApiDataDrivenTest.java
@@ -1,32 +1,14 @@
 package com.api.test.datadriven;
 
-import static io.restassured.module.jsv.JsonSchemaValidator.*;
+import static com.api.utils.SpecUtil.requestSpecWithAuth;
+import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 
 import org.hamcrest.Matchers;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-import com.api.constant.Model;
-import com.api.constant.OEM;
-import com.api.constant.Platform;
-import com.api.constant.Problems;
-import com.api.constant.ServiceLocation;
 import com.api.constant.UserRole;
-import com.api.constant.WarrentyStatus;
-import com.api.constant.product;
 import com.api.request.model.CreateJobPayload;
-import com.api.request.model.Customer;
-import com.api.request.model.CustomerAddress;
-import com.api.request.model.CustomerProduct;
-import com.api.request.model.Problem;
-import static com.api.utils.DateTimeProvider.getDateAndTimeDaysAgo;
-import static com.api.utils.SpecUtil.*;
-
-import static io.restassured.RestAssured.*;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class CreateJobApiDataDrivenTest {
 		
