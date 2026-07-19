db02120daa84b19c44798e5225f1f4bdf6e3b449
praveen <psagra13@gmail.com>
2026-07-20 02:26:28 +0530

    added the cache for token provider utils

--- src/test/java/com/api/utils/AuthTokenProvider.java
+++ src/test/java/com/api/utils/AuthTokenProvider.java
@@ -2,18 +2,30 @@ package com.api.utils;
 
 import static io.restassured.RestAssured.given;
 
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 import com.api.constant.UserRole;
 import com.api.request.model.UserCredencials;
 
 import io.restassured.http.ContentType;
 
 public class AuthTokenProvider {
+	private static Map<UserRole, String> tokenCace = new ConcurrentHashMap<UserRole, String>();
+	private static String token;
 
 	private AuthTokenProvider() {
 		// private constructor to prevent instantiation
 	}
-
+	
+	
 	public static String getToken(UserRole role) {
+		if(tokenCace.containsKey(role)) {
+			       return tokenCace.get(role);
+			
+		}
+		
+		else {
 		UserCredencials userCredentials = switch (role) {
 		case FD -> new UserCredencials("iamfd", "password");
 		case SUP -> new UserCredencials("iamsup", "password");
@@ -22,10 +34,12 @@ public class AuthTokenProvider {
 		default -> throw new IllegalArgumentException("Invalid role: " + role);
 		};
 
-		return given().baseUri(ConfigManager.getProperty("BASE_URI")).accept(ContentType.JSON)
-				.contentType(ContentType.JSON).body(userCredentials).log().all().when().post("/login").then().log()
-				.all().extract().body().jsonPath().getString("data.token");
-		
+	token=given().baseUri(ConfigManager.getProperty("BASE_URI")).accept(ContentType.JSON)
+				.contentType(ContentType.JSON).body(userCredentials).when().post("/login").then().log()
+				.ifError().extract().body().jsonPath().getString("data.token");
+		   tokenCace.put(role, token);
+		   return token;	
 
 	}
+}
 }
